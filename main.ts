ccf64b2127638bc516c5f955ba69b1f059a1cdd4
unxok <anderluke707@gmail.com>
2024-08-20 03:12:43 -0700

    Working pretty decently, just need to add extra features

--- main.ts
+++ main.ts
@@ -1,4 +1,15 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Component,
+	Editor,
+	MarkdownRenderer,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
 
 // Remember to rename these classes and interfaces!
 
@@ -7,8 +18,8 @@ interface MyPluginSettings {
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	mySetting: "default",
+};
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
@@ -16,94 +27,63 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.addCommand({
+			id: "ukutabs-importer:import-song",
+			name: "Ukutabs importer: Import song",
+			callback: () => {
+				const modal = new ImportSongModal(this.app);
+				modal.open();
+			},
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerMarkdownCodeBlockProcessor(
+			"ukutabs",
+			async (source, el, ctx) => {
+				const file = this.app.vault.getFileByPath(ctx.sourcePath);
+				if (!file) {
+					// TODO handle this better
+					console.error(
+						"Could not find file for corresponding codeblock"
+					);
+					return;
+				}
+				const info = this.app.metadataCache.getFileCache(file);
+				const chordLinks = info?.links
+					?.filter((l) => l.link.endsWith(".svg"))
+					?.map((l) => l.original);
+				const uniqueChordLinks = [...new Set(chordLinks)];
+				const embeds =
+					uniqueChordLinks?.reduce((acc, cur) => {
+						return acc + " !" + cur;
+					}, "") ?? "No chords found";
+				const cmp = new Component();
+				el.empty();
+				el.classList.add("ukutabs-importer-chords-codeblock");
+				MarkdownRenderer.render(
+					this.app,
+					embeds,
+					el,
+					ctx.sourcePath,
+					cmp
+				);
+			}
+		);
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
@@ -116,19 +96,171 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
+		const { containerEl } = this;
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Setting #1")
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					})
+			);
+	}
+}
+
+class ImportSongModal extends Modal {
+	private url: string = "";
+	private fileName: string = "";
+	private folderPath: string = "/";
+	private fileNameError: HTMLLIElement;
+	private urlError: HTMLLIElement;
+	constructor(app: App) {
+		super(app);
+	}
+
+	onOpen(): void {
+		const { contentEl } = this;
+		// containerEl.empty();
+		contentEl.createEl("h2", { text: "Import Ukutabs song" });
+		const fileNameError = createEl("li", {
+			text: "",
+			cls: "ukutabs-importer-hidden",
+		});
+		const urlError = createEl("li", {
+			text: "",
+			cls: "ukutabs-importer-hidden",
+		});
+		this.fileNameError = fileNameError;
+		this.urlError = urlError;
+		const ul = createEl("ul");
+		ul.appendChild(fileNameError);
+		ul.appendChild(urlError);
+		contentEl.appendChild(ul);
+		new Setting(contentEl)
+			.setName("URL")
+			.setDesc("The full URL to the song, including 'https://'.")
+			.addText((cmp) => {
+				cmp.onChange((v) => (this.url = v));
+			});
+		new Setting(contentEl)
+			.setName("File name")
+			.setDesc(
+				"The name of the file that is created. Do NOT include the file extension."
+			)
+			.addText((cmp) => {
+				cmp.onChange((v) => (this.fileName = v));
+			});
+		new Setting(contentEl)
+			.setName("Folder path")
+			.setDesc("The folder to save the new file to.")
+			.addDropdown((cmp) => {
+				const folders = this.app.vault.getAllFolders(true);
+				const obj = folders.reduce((acc, cur) => {
+					const folder = cur.path;
+					acc[folder] = folder;
+					return acc;
+				}, {} as Record<string, string>);
+				cmp.addOptions(obj);
+				cmp.setValue("/");
+				cmp.onChange((v) => (this.folderPath = v));
+			});
+
+		new Setting(contentEl).addButton((cmp) => {
+			cmp.setButtonText("import");
+			cmp.onClick(() => {
+				const isDup = this.isDuplicateFile();
+				const isUkuUrl = this.isUkutabURL();
+				if (isDup) {
+					this.fileNameError.setText(
+						"File of this name already exists in this folder!"
+					);
+					this.fileNameError.setAttribute(
+						"class",
+						"ukutabs-importer-text-error"
+					);
+				} else {
+					this.fileNameError.setText("");
+					this.fileNameError.setAttribute(
+						"class",
+						"ukutabs-importer-hidden"
+					);
+				}
+				if (!isUkuUrl) {
+					this.urlError.setText(
+						"This is not a valid Ukutabs.com URL!"
+					);
+					this.urlError.setAttribute(
+						"class",
+						"ukutabs-importer-text-error"
+					);
+				} else {
+					this.urlError.setText("");
+					this.urlError.setAttribute(
+						"class",
+						"ukutabs-importer-hidden"
+					);
+				}
+				if (isDup || !isUkuUrl) return;
+				this.importSong();
+			});
+		});
+
+		// new Setting(containerEl).setN
+	}
+
+	getFilePath(): string {
+		const { fileName, folderPath } = this;
+		const fp = folderPath === "/" ? "" : folderPath + "/";
+		const path = fp + fileName + ".md";
+		return path;
+	}
+
+	isDuplicateFile(): boolean {
+		const path = this.getFilePath();
+		const file = this.app.vault.getFileByPath(path);
+		if (file) return true;
+		return false;
+	}
+
+	isUkutabURL(): boolean {
+		return this.url.startsWith("https://ukutabs.com/");
+	}
+
+	async importSong(): Promise<void> {
+		const res = await fetch(this.url);
+		const text = await res.text();
+		const parser = new DOMParser();
+		const doc = parser.parseFromString(text, "text/html");
+		const content = doc.getElementById("ukutabs-song");
+		if (!content) {
+			// TODO handle this better
+			throw new Error(
+				"Could not find element which contains lyrics and chords"
+			);
+		}
+		const { children } = content;
+		if (!children) {
+			// TODO handle this better
+			throw new Error(
+				"Content appears to empty... this should never happen"
+			);
+		}
+		Array.from(children).forEach((el, index) => {
+			const tag = el.tagName.toLowerCase();
+			const chord = el.textContent;
+			if (tag === "a") {
+				el.outerHTML = "[[" + chord + ".svg|" + chord + "]]";
+			}
+		});
+
+		const file = await this.app.vault.create(
+			this.getFilePath(),
+			content.innerHTML
+		);
 	}
 }
